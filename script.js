41f84d6fe601c2ce13ef928c8947d527d07b7ab8
AnujGahra <anujgahra244304@gmail.com>
2025-03-04 10:46:23 +0530

    redux

--- script.js
+++ script.js
@@ -1,83 +1,94 @@
 import { createStore } from "redux";
-import { myCreateStore } from "./my-redux";
-const postCountElement = document.querySelector(".post-count");
+import { productsList } from "./productList";
 
 const initialState = {
-  post: 0,
-  name: "Anuj Kumar",
-  age: 22,
+  products: productsList,
+  cartItems: [],
+  wishList: [],
 };
 
-const INCREMENT = "post/increment";
-const DECREMENT = "post/decrement";
-const INCREASE_BY = "post/incrementBy";
-const DECREASE_BY = "post/decrementBy";
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
+const CART_ITEM_INCREASE_QUANTITY = "cart/increaseItemQuantity";
+const CART_ITEM_DECREASE_QUANTITY = "cart/decreaseItemQuantity";
+
+const WISHLIST_ADD_ITEM = "wishList/addItem";
+const WISHLIST_REMOVE_ITEM = "wishList/removeItem";
 
 function reducer(state = initialState, action) {
-  // if (action.type === INCREMENT) {
-  //   return { ...state, post: state.post + 1 };
-  // } else if (action.type === DECREMENT) {
-  //   return { ...state, post: state.post - 1 };
-  // }
-  //   else if (action.type === INCREASE_BY) {
-  //   return { ...state, post: state.post + action.payload };
-  // }
-  //   else if (action.type === DECREASE_BY) {
-  //   return { ...state, post: state.post - action.payload };
-  // }
-  // return state;
-
-  // Switch case
   switch (action.type) {
-    case INCREMENT:
-      return { ...state, post: state.post + 1 };
-    case DECREMENT:
-      return { ...state, post: state.post - 1 };
-    case INCREASE_BY:
-      return { ...state, post: state.post + action.payload };
-    case DECREASE_BY:
-      return { ...state, post: state.post - action.payload };
+    case CART_ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.productId !== action.payload.productId
+        ),
+      };
+    case CART_ITEM_INCREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.productId === action.payload.productId) {
+            return { ...cartItem, quantity: cartItem.quantity + 1 };
+          }
+          return cartItem;
+        }),
+      };
+    case CART_ITEM_DECREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems
+          .map((cartItem) => {
+            if (cartItem.productId === action.payload.productId) {
+              return { ...cartItem, quantity: cartItem.quantity - 1 };
+            }
+            return cartItem;
+          })
+          .filter((cartItem) => cartItem.quantity > 0),
+      };
+
+    case WISHLIST_ADD_ITEM:
+      return { ...state, wishList: [...state.wishList, action.payload] };
+
+    case WISHLIST_REMOVE_ITEM:
+      return {
+        ...state,
+        wishList: state.wishList.filter(
+          (wishListItem) => wishListItem.productId !== action.payload.productId
+        ),
+      };
     default:
       return state;
   }
 }
 
-// What redux will do
-// reduxState = reducer(reduxState, { type: "post/increment" });
-// console.log(reduxState)
-// reduxState = reducer(reduxState, { type: "post/increment" });
-// console.log(reduxState)
-// reduxState = reducer(reduxState, { type: "post/decrement" });
-// console.log(reduxState)
-// reduxState = reducer(reduxState, { type: "post/incrementBy", payload: 10 });
-// console.log(reduxState);
-// reduxState = reducer(reduxState, { type: "post/decrementBy", payload: 10 });
-// console.log(reduxState);
-
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
-const myStore = myCreateStore(reducer);
+
 console.log(store);
-console.log(myStore);
-// console.log(store.getState())
-myStore.subscribe(() => {
-  console.log(myStore.getState());
-  postCountElement.innerText = myStore.getState().post;
+
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } });
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productId: 12, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ITEM_INCREASE_QUANTITY,
+  payload: { productId: 12 },
+});
+store.dispatch({
+  type: CART_ITEM_DECREASE_QUANTITY,
+  payload: { productId: 12 },
 });
 
-postCountElement.innerText = store.getState().post;
-
-myStore.dispatch({ type: INCREMENT });
-// console.log(store.getState())
-myStore.dispatch({ type: DECREMENT });
-// console.log(store.getState())
-
-myStore.dispatch({ type: INCREASE_BY, payload: 10 });
-myStore.dispatch({ type: DECREASE_BY, payload: 10 });
-
-// setTimeout(() => {
-//   myStore.dispatch({type: INCREMENT})
-// }, 2000);
-
-postCountElement.addEventListener("click", () => {
-  myStore.dispatch({ type: INCREMENT });
+store.dispatch({
+  type: CART_ITEM_DECREASE_QUANTITY,
+  payload: { productId: 12 },
 });
+
+store.dispatch({ type: WISHLIST_ADD_ITEM, payload: { productId: 18 } });
+store.dispatch({ type: WISHLIST_ADD_ITEM, payload: { productId: 11 } });
+store.dispatch({ type: WISHLIST_REMOVE_ITEM, payload: { productId: 11 } });
+store.dispatch({ type: WISHLIST_REMOVE_ITEM, payload: { productId: 18 } });
+console.log(store.getState());
