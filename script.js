49b9650df51118587d1435501abdd4383e890ea9
AnujGahra <anujgahra244304@gmail.com>
2025-03-04 07:17:35 +0530

    fundamental of Redux

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
-let reduxState = {
+import {createStore} from "redux"
+
+const initialState = {
   post: 0,
   name: "Anuj Kumar",
   age: 22,
 };
 
-function reducer(state, action) {
-  if (action.type === "post/increment") {
-    return { ...state, post: state.post + 1 };
-  } else if (action.type === "post/decrement") {
-    return { ...state, post: state.post - 1 };
+const INCREMENT = "post/increment";
+const DECREMENT = "post/decrement";
+const INCREASE_BY = "post/incrementBy";
+const DECREASE_BY = "post/decrementBy";
+
+function reducer(state = initialState, action) {
+  // if (action.type === INCREMENT) {
+  //   return { ...state, post: state.post + 1 };
+  // } else if (action.type === DECREMENT) {
+  //   return { ...state, post: state.post - 1 };
+  // }
+  //   else if (action.type === INCREASE_BY) {
+  //   return { ...state, post: state.post + action.payload };
+  // }
+  //   else if (action.type === DECREASE_BY) {
+  //   return { ...state, post: state.post - action.payload };
+  // }
+  // return state;
+
+  // Switch case
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, post: state.post + 1 };
+    case DECREMENT:
+      return { ...state, post: state.post - 1 };
+    case INCREASE_BY:
+      return { ...state, post: state.post + action.payload };
+    case DECREASE_BY:
+      return { ...state, post: state.post - action.payload };
+    default:
+      return state;
   }
-    else if (action.type === "post/incrementBy") {
-    return { ...state, post: state.post + action.payload };
-  }
-    else if (action.type === "post/decrementBy") {
-    return { ...state, post: state.post - action.payload };
-  }
-  return state;
 }
 
 // What redux will do
-reduxState = reducer(reduxState, { type: "post/increment" });
-console.log(reduxState)
-reduxState = reducer(reduxState, { type: "post/increment" });
-console.log(reduxState)
-reduxState = reducer(reduxState, { type: "post/decrement" });
-console.log(reduxState)
-reduxState = reducer(reduxState, { type: "post/incrementBy", payload: 10 });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/decrementBy", payload: 10 });
-console.log(reduxState);
+// reduxState = reducer(reduxState, { type: "post/increment" });
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: "post/increment" });
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: "post/decrement" });
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: "post/incrementBy", payload: 10 });
+// console.log(reduxState);
+// reduxState = reducer(reduxState, { type: "post/decrementBy", payload: 10 });
+// console.log(reduxState);
 
 
-// 1:05:00 more remains
+
+const store = createStore(reducer)
+console.log(store)
+// console.log(store.getState())
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({type:INCREMENT})
+// console.log(store.getState())
+store.dispatch({type: DECREMENT})
+// console.log(store.getState())
+
+store.dispatch({type: INCREASE_BY, payload:10})
+store.dispatch({type: DECREASE_BY, payload:11})
