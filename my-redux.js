3c2fb2b597fcfdd37e5d4d85ef5e0cedadc18f74
AnujGahra <anujgahra244304@gmail.com>
2025-03-04 09:12:41 +0530

    own redux

--- my-redux.js
+++ my-redux.js
@@ -1,26 +1,26 @@
-export function myCreateStore(reducer){
-    let state;
-    const listeners = [];
-    const store = {
-        getState(){
-            return state
-        },
-        dispatch(action) {
-            state = reducer(state, action);
-            listeners.forEach((listener) => {
-                listener()
-            })
-        },
-        subscribe(listener) {
-            listeners.push(listener)
-            return function() {
-                const listenerIndex = listeners.findIndex(
-                    (registeredListeners) => registeredListeners === listener
-                )
-                listener.splice(listenerIndex, 1);
-            }
-        }
-    }
-    store.dispatch({type: '@@INIT'})
-    return store;
+export function myCreateStore(reducer) {
+  let state;
+  const listeners = [];
+  const store = {
+    getState() {
+      return state;
+    },
+    dispatch(action) {
+      state = reducer(state, action);
+      listeners.forEach((listener) => {
+        listener();
+      });
+    },
+    subscribe(listener) {
+      listeners.push(listener);
+      return function () {
+        const listenerIndex = listeners.findIndex(
+          (registeredListeners) => registeredListeners === listener
+        );
+        listener.splice(listenerIndex, 1);
+      };
+    },
+  };
+  store.dispatch({ type: "@@INIT" });
+  return store;
 }
